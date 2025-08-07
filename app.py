c756c594f8aa74432a27fb2f5ba92c88d11e8f74
rochamariza <marizanfmrocha04@gmail.com>
2025-08-07 18:02:01 -0300

    update .gitignore

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from routes.academia import academias_route
 from routes.sobre import sobre_route
 from routes.home import home_route
 
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///suaacademia.db'
+db = SQLAlchemy(app)
 app.register_blueprint(home_route)
 app.register_blueprint(academias_route)
 app.register_blueprint(sobre_route)
 
-
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ routes/usuario.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+sobre_route = Blueprint('sobre', __name__)
+
+@sobre_route.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
